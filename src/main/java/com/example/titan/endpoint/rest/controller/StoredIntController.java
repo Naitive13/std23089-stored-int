0c487b6d4bfa66384e658eb96878567808684580
Naitive13 <hei.titan.2@gmail.com>
2025-07-07 18:51:06 +0300

    feat: add endpoint GET /stored-int

---
+++ src/main/java/com/example/titan/endpoint/rest/controller/StoredIntController.java
@@ -0,0 +1,19 @@
+package com.example.titan.endpoint.rest.controller;
+
+
+import com.example.titan.service.StoredIntService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class StoredIntController {
+    public final StoredIntService storedIntService;
+    @GetMapping("/stored-int")
+    public ResponseEntity getStoredInt(){
+        String result = storedIntService.getStoredInt();
+        return ResponseEntity.ok().body(result);
+    }
+}
