94d5a8cd358c53fab9286b4e5b0af063db9a41b3
Naitive13 <hei.titan.2@gmail.com>
2025-07-07 18:50:53 +0300

    feat: create service to create/get the random int

---
+++ src/main/java/com/example/titan/service/StoredIntService.java
@@ -0,0 +1,29 @@
+package com.example.titan.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+@Slf4j
+public class StoredIntService {
+    public String getStoredInt(){
+        String filename = "stored-int.txt";
+        int content = 0;
+        try (Scanner file = new Scanner(filename)){
+            while (file.hasNextLine()){
+                content = Integer.parseInt(file.nextLine());
+            }
+            return "Read value from file: " + content;
+        } catch (Exception e){
+            try (FileWriter file = new FileWriter(filename)){
+                content = (int) (Math.random()*10);
+                file.write(content);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        return "Created new file with value: " + content;
+    }
+}
